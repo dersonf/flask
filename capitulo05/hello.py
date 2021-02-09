cf3a3bdba3c856401ea5f0e0b1e550b9328c3031
Anderson Ferneda <aferneda@uolinc.com>
2021-02-09 08:58:54 -0300

    Modelando base de dados

--- capitulo05/hello.py
+++ capitulo05/hello.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from  wtforms.validators import DataRequired
+from wtforms.validators import DataRequired
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -23,6 +23,26 @@ bootstrap = Bootstrap(app)
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 @app.route('/', methods=['GET', 'POST'])
