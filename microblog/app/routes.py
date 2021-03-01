670bec15a30aa6c1fc204cd012d9048d427a839a
Anderson <anderson@pinguim.local>
2021-03-01 10:22:48 -0300

    Basic form

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -18,3 +19,15 @@ def index():
     ]
     return render_template(
         'index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    '''Se for GET recebe False, se for POST recebe True e valida o form
+    se der algum erro retorna False'''
+    if form.validate_on_submit():
+        # Exibe mensagem para o usuário, o template precisa de ajustes
+        flash(f"Login requestd for user {form.username.data}, remember_me={form.remember_me.data}")
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
