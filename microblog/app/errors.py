6f9949875cb66f29de227ebb8476e6b6b81277f4
Anderson <anderson@pinguim.local>
2021-03-08 09:38:59 -0300

    user duplicated

---
+++ microblog/app/errors.py
@@ -0,0 +1,15 @@
+from flask import render_template
+from app import app, db
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    'Tela customizada para o erro 404'
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    'Tela customizada para o erro 500'
+    db.session.rollback()
+    return render_template('500.html'), 500
