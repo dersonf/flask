b0d8159d3a85b6567bda188177b778ab04857b92
Anderson <anderson@pinguim.local>
2021-03-10 09:37:46 -0300

    Seguidores e testes

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -7,6 +7,11 @@ from flask_login import UserMixin
 from app import db
 from app import login
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 
 class User(UserMixin, db.Model):
     'Como a classe chama User a tabela terá o nome user'
@@ -19,6 +24,12 @@ class User(UserMixin, db.Model):
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     # Isso é uma view
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic'
+    )
 
     # Ao criar usuário ele cospe ele na saída padrão
     def __repr__(self):
@@ -36,6 +47,35 @@ class User(UserMixin, db.Model):
         'Gera url para o avatar em gravatar'
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f"https://www.gravatar.com/avatar/{digest}?d=robohash&r=x&s={size}"
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        # Mostra os posts que o usuário segue mas não os próprios
+        # return Post.query.join(
+            # Monta a tabela de seguidores e posts, filtra e ordena
+            # followers, (followers.c.followed_id == Post.user_id)).filter(
+            #     followers.c.follower_id == self.id).order_by(
+            #         Post.timestamp.desc())
+        # Busca os seguidos
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        # Busca os próprios posts
+        own = Post.query.filter_by(user_id = self.id)
+        # Faz a união e ordena
+        return followed.union(own).order_by(Post.timestamp.desc())
+        
 
 
 class Post(db.Model):
