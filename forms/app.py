64820d8d6a1b40f0d474de216ad57918b26dc778
Anderson <anderson@pinguim.local>
2021-03-28 19:30:00 -0300

    Teste com StringField

--- forms/app.py
+++ forms/app.py
@@ -1,33 +1,19 @@
 from flask import Flask, render_template, redirect, url_for, session
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms.validators import DataRequired, ValidationError, InputRequired
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
-
-class Name(FlaskForm):
-    nome = StringField('nome', validators=[DataRequired()])
+class NameForm(FlaskForm):
+    nome = StringField('nome', validators=[DataRequired(message='Precisa ser preenchido')], _prefix='ttt')
     submit = SubmitField('OK')
 
-    def check_nome(form, nome):
-        if not nome.data:
-            raise ValidationError('Teste')
+    def validate_nome(form, field):
+        if len(field.data) < 3:
+            raise ValidationError('Nome só com duas letras?')
 
 @app.route('/')
 def index():
@@ -36,13 +22,17 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def nome():
-    form = Name()
+    form = NameForm()
     if form.validate_on_submit():
         # url_for vai usar a função ao inves do path
         # return redirect('/')
         session['nome'] = form.nome.data
         return redirect(url_for('index'))
     return render_template('login.html', title='Login', form=form)
+
+# @app.route('/checkbox', methods=['GET', 'POST'])
+# def checkbox():
+#     form
 
 
 @app.route('/logout')
